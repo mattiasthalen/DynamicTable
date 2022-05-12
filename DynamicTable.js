18a50eacf0198d78ff7cc7ad1a79ccc65b71f57e
Mattias Thalén <mattias.thalen@pm.me>
2022-05-12 23:46:23 +0200

    Change single quotes to double quotes

--- DynamicTable.js
+++ DynamicTable.js
@@ -47,17 +47,17 @@ define(["qlik", "jquery"],
 
             for (let i = 0; i < maxFields; i++) {
                 var n = i + 1;
-                var baseFormula = 'SubField(Concat([' + dimensionField + '], Chr(124)), Chr(124), ' + n + ')';
+                var baseFormula = "SubField(Concat([" + dimensionField + "], Chr(124)), Chr(124), " + n + ")";
 
                 var columnData = {
                     qDef: {
                         qFieldDefs: [
-                            '=$(=Chr(91) & ' + baseFormula + ' & Chr(93))'
+                            "=$(=Chr(91) & " + baseFormula + " & Chr(93))"
                         ],
-                        qLabelExpression: '=' + baseFormula
+                        qLabelExpression: "=" + baseFormula
                     },
                     qCalcCondition: {
-                        qCond: '=GetSelectedCount([' + dimensionField + ']) >= ' + n
+                        qCond: "=GetSelectedCount([" + dimensionField + "]) >= " + n
                     }
                 };
 
@@ -96,7 +96,7 @@ define(["qlik", "jquery"],
                         })
                         .then(function (sheetObj) {
                             sheetObj.properties.cells.forEach(function (cell) {
-                                if (cell.name == initialId) cell.type = 'table';
+                                if (cell.name == initialId) cell.type = "table";
                             });
                             return sheetObj.setProperties(sheetObj.properties);
                         })
@@ -114,14 +114,11 @@ define(["qlik", "jquery"],
             },
             definition: settings,
             paint: function ($element, layout) {
-
-                console.info('paint >> layout >> ', layout);
-
                 $element.html(
-                    '<table height="100%"><tr><td style="text-align:center;">'
-                    + '<p>This is a placeholder for a dynamic table.</p>'
-                    + '<p>After configuring the setting this will become a standard Qlik Sense table.</p>'
-                    + '<td></tr></table>');
+                    "<table height='100%'><tr><td style='text-align:center;'>"
+                    + "<p>This is a placeholder for a dynamic table.</p>"
+                    + "<p>After configuring the setting this will become a standard Qlik Sense table.</p>"
+                    + "<td></tr></table>");
 
                 return qlik.Promise.resolve();
             }
